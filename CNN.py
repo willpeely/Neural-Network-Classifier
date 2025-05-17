c06f4c157c16a43b6bd304f6af6a0cc0a79b10a2
willpeely <william.e.peel@icloud.com>
2025-05-17 19:26:05 +0000

    Creating convolutional layers

--- CNN.py
+++ CNN.py
@@ -11,3 +11,32 @@ convolutional_stride = 1
 convolutional_padding = 1
 convolutional_activation = nn.ReLU()
 convolutional_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+# Used to create a convolutional layer with defined input and output channels
+def convolutional_layer(input_channels, output_channels, pool=True):
+    """ Returns a convolutional layer.
+
+    Args:
+        input_channels (int): The number of inputs into the layer.
+        output_channels (int): The number of outputs the layer produces.
+        pool (bool, optional): If True the convolutional layer will use pooling. Defaults to True.
+    """
+    layers = [
+        nn.Conv2d(
+            in_channels=input_channels, 
+            out_channels=output_channels,
+            kernel_size=convolutional_kernel_size,
+            stride=convolutional_stride,
+            padding=convolutional_padding
+        ),
+
+        nn.BatchNorm2d(
+            num_features=output_channels
+        ),
+
+        convolutional_activation
+    ]
+    if pool:
+        layers.append(convolutional_pool)
+
+    return nn.Sequential(*layers)
